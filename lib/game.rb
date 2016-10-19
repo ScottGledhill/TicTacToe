5a2d93a1c2695b0bc976d9c445f7ffc49bbe4f49
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-10-19 17:56:21 +0100

    game won logic correct and refactored

--- lib/game.rb
+++ lib/game.rb
@@ -16,7 +16,6 @@ class Game
   def move(move)
     if BOARD.include?(move)
       @board.map! {|e| e == move ? @current_turn : e}
-      rotate_turn
       game_won?
     else
       raise error
@@ -31,26 +30,30 @@ class Game
   private
 
   def game_won?
-    if board[0] == board[1] && board[1] == board[2]
-    elsif board[6] == board[7] && board[7] == board[8]
-    elsif board[3] == board[4] && board[4] == board[5]
-    elsif board[1] == board[4] && board[4] == board[7]
-    elsif board[0] == board[3] && board[3] == board[6]
-    elsif board[1] == board[4] && board[4] == board[7]
-      diagonal_win
-    end
+    vertical_win? || horizontal_win? || diagonal_win? ? "#{current_turn} won" : false
   end
 
   def vertical_win?
+    if board[1] == board[4] && board[4] == board[7] ||
+       board[0] == board[3] && board[3] == board[6] ||
+       board[1] == board[4] && board[4] == board[7]
+       true
+    end
   end
 
   def horizontal_win?
+    if board[0] == board[1] && board[1] == board[2] ||
+       board[6] == board[7] && board[7] == board[8] ||
+       board[3] == board[4] && board[4] == board[5]
+       true
+    end
   end
 
   def diagonal_win?
-    if board[2] == board[5] && board[5] == board[8]
-      elsif board[0] == board[4] && board[4] == board[8]
-      elsif board[6] == board[4] && board[4] == board[2]
+    if board[2] == board[5] && board[5] == board[8]||
+      board[0] == board[4] && board[4] == board[8] ||
+      board[6] == board[4] && board[4] == board[2]
+      true
     end
   end
 
